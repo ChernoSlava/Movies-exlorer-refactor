3c579173896ef513e8efe0d7c2d6cb15d1328ac0
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-03-10 00:16:20 +0300

    feat(TS/components/main):
    - Use TS in main/Promo component.
    
    meta: Level-3

--- src/components/Main/Promo/Promo.tsx
+++ src/components/Main/Promo/Promo.tsx
@@ -4,7 +4,7 @@ import { NavTab } from '../NavTab';
 
 import { PromoScreen, PromoStyled, PromoTitle } from './styled';
 
-export function Promo() {
+export const Promo: React.FC = (): JSX.Element => {
   return (
     <PromoStyled>
       <PromoScreen>
--- src/components/Main/Promo/__stories__/Promo.stories.tsx
+++ src/components/Main/Promo/__stories__/Promo.stories.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { Promo } from '../Promo';
 
 export default {
   title: 'Example/Promo',
   component: Promo,
-};
+} as ComponentMeta<typeof Promo>;
 
-const Template = args => <Promo {...args} />;
+const Template: ComponentStory<typeof Promo> = args => <Promo {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
